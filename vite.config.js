0d4cdac956935a2edb6c1bb09c9c1fdba833db37
HazanOmri <omrihazan1313@gmail.com>
2023-02-15 14:02:08 +0200

    adding dist subtree

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/Mister-Bitcoin/" : "/",
   plugins: [vue()],
+  publicPath: process.env.NODE_ENV === "production" ? "/Mister-Bitcoin/" : "/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
